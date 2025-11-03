757755de5322278ecca8e4375abd804310e37aa7
Manuel <blackonyxx20.4@gmail.com>
2025-11-03 13:24:34 +0300

    Better theme

--- src/main/Board.java
+++ src/main/Board.java
@@ -5,7 +5,7 @@ import java.awt.*;
 
 public class Board extends JPanel {
 
-    public int tileSize = 85;
+    public int tileSize = 80;
     int cols = 8;
     int rows = 8;
 
@@ -18,7 +18,7 @@ public class Board extends JPanel {
         Graphics2D g2d = (Graphics2D) g;
         for(int r = 0; r < rows; r++)
             for(int c = 0; c < cols; c++){
-                g2d.setColor((c+r)%2 == 0? Color.gray: Color.darkGray);
+                g2d.setColor((c+r)%2 == 0? new Color(199, 170, 120): new Color(131, 92, 56));
                 g2d.fillRect(c * tileSize, r * tileSize, tileSize, tileSize);
             }
     }
--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,7 @@ import java.awt.*;
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
+        frame.getContentPane().setBackground(Color.BLACK);
         frame.setLayout(new GridBagLayout());
         frame.setMinimumSize(new Dimension(1000, 1000));
         frame.setLocationRelativeTo(null);
